2124aab83844d79add7d8a260b81ddc4b11dacaa
arneke <arneke@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2006-12-01 19:58:38 +0000

    Commit test

--- receiverDemo/ReceiverDemo.java
+++ receiverDemo/ReceiverDemo.java
@@ -43,8 +43,6 @@ public class ReceiverDemo implements RTPAppIntf {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
 		System.out.println("Setup");
 		ReceiverDemo aDemo = new ReceiverDemo("Test",45455,"127.0.0.1");
 		while(aDemo.nBytesRead == 0) {
