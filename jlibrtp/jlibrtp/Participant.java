870863e0f18a2dc63272a71eb3d8d43dbad5fda4
arneke <arneke@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2006-12-17 00:08:12 +0000

    code sync

--- jlibrtp/jlibrtp/Participant.java
+++ jlibrtp/jlibrtp/Participant.java
@@ -1,4 +1,3 @@
-package jlibrtp;
 /**
  * Java RTP Library
  * Copyright (C) 2006 Vaishnav Janardhan / Arne Kepp
@@ -17,10 +16,13 @@ package jlibrtp;
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+package jlibrtp;
 
 import java.net.InetAddress;
 
+/**
+ * A participant represents a peer in an RTPSession. Based on the information stored on these objects, packets are processed and statistics generated for RTCP.
+ */
 public class Participant {
 	private int rtpDestPort = 0;
 	private int rtcpDestPort = 0;
@@ -31,14 +33,28 @@ public class Participant {
 	public String cname = null;
 	public int	lastRecvSeqNumber = 0;
 	public int	lostPktCount = 0;
-	public int	octetCount = 0;
+	public int	recvOctetCount = 0;
+	public int  recvPktCount = 0;
+	public int	sendOctetCount = 0;
+	public int  sendPktCount = 0;
 	long lastRecvTimeStamp;
-	
 	
 	//Store the packets received from this participant
 	public PktBuffer pktBuffer = null;
 
-	// Known contact, but we don't know their ssrc yet.
+
+	/**
+	 * Create a basic participant. If this is a <b>unicast</b> session you must provide network address (ipv4 or ipv6) and ports for RTP and RTCP, 
+	 * as well as a cname for this contact. These things should be negotiated through SIP or a similar protocol.
+	 * 
+	 * If this is a <b>multicast</b> session you should set the ports and networkAddress to 0, 0, 127.0.0.1 (they will be ignored). 
+	 * 
+	 * jlibrtp will listen for RTCP packets to obtain a matching SSRC for this participant, based on cname.
+	 * @param networkAddress string representation of network address (ipv4 or ipv6). Use "127.0.0.1" for multicast session.
+	 * @param rtpPort port on which peer expects RTP packets. Use 0 if this is a sender-only, or this is a multicast session.
+	 * @param rtcpPort port on which peer expects RTCP packets. Use 0 if this is a sender-only, or this is a multicast session.
+	 * @param CNAME the cname of the peer, we expect this to be included in his RTCP SDES messages.
+	 */
 	public Participant(String networkAddress, int rtpPort, int rtcpPort, String CNAME) {
 		if(RTPSession.rtpDebugLevel > 6) {
 			System.out.println("New participant created: " + CNAME + "@" + networkAddress);
@@ -53,6 +69,7 @@ public class Participant {
 		rtpDestPort = rtpPort;
 		rtcpDestPort = rtcpPort;
 	}
+	
 	// Incomplete insert, we got a packet, but we don't know this person yet.
 	public Participant(InetAddress adr, int rtpPort, long SSRC) {
 		address = adr;
@@ -60,43 +77,92 @@ public class Participant {
 		ssrc = SSRC;
 	}
 	
+	/**
+	 * Toggle whether this participant is expected to send packets to us or not.
+	 * 
+	 * @param doesSend true if we expect packets from this participant.
+	 */
 	public void isSender(boolean doesSend) {
 		isSender = doesSend;
 	}
 
+	/**
+	 * Check whether we expect packets from this participant or not.
+	 * 
+	 * @return true if we expect packets from this source.
+	 */
 	public boolean isSender() {
 		return isSender;
 	}
 	
+	/**
+	 * Toggle whether this participant should receive packets from us or not.
+	 * 
+	 * @param doesReceive true if we should send packets to this participant.
+	 */
 	public void isReceiver(boolean doesReceive) {
 		isReceiver = doesReceive;
 	}
 
+	/**
+	 * Check whether this participant expects packets from us or not.
+	 * 
+	 * @return true if the receiver expects packets from us.
+	 */
 	public boolean isReceiver() {
 		return isReceiver;
 	}
 	
+	/**
+	 * Check whether this participant expects packets from us or not.
+	 * 
+	 * @return true if the receiver expects packets from us.
+	 */
 	int getRtpDestPort() {
 		return rtpDestPort;
 	}
 	
+	/**
+	 * Check whether this participant expects packets from us or not.
+	 * 
+	 * @return true if the receiver expects packets from us.
+	 */
 	int getRtcpDestPort() {
 		return rtcpDestPort;
 	}
 	
+	/**
+	 * Address registered with this participant.
+	 * 
+	 * @return address of participant
+	 */
 	InetAddress getInetAddress() {
 		return address;
 	}
 	
+	/**
+	 * Address registered with this participant.
+	 * 
+	 * @return address of participant
+	 */
 	public String getCNAME() {
 		return cname;
 	}
 	
-	
+	/**
+	 * SSRC for participant, determined through RTCP SDES
+	 * 
+	 * @return SSRC (32 bit unsigned integer)
+	 */
 	public long getSSRC() {
 		return this.ssrc;
 	}
 	
+	/**
+	 * SSRC for participant, determined through RTCP SDES
+	 * 
+	 * @return SSRC (32 bit unsigned integer)
+	 */
 	public int setSSRC(long anSSRC) {
 		if(ssrc < 0) {
 			return -1;
@@ -105,6 +171,7 @@ public class Participant {
 			return 0;
 		}
 	}
+
 	// Check ParticipantDatabase.java if you change this!
 	public long simpleHash() {
 		if(ssrc > 0) {
