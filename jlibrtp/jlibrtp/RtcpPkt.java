0d554c6b622171a183886191a56ed08a3ffbf439
arneke <arneke@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2007-02-01 07:57:11 +0000

    RTCP start

--- jlibrtp/jlibrtp/RtcpPkt.java
+++ jlibrtp/jlibrtp/RtcpPkt.java
@@ -1,17 +1,32 @@
 package jlibrtp;
 
 public class RtcpPkt {
-	private boolean rawPktCurrent = false;
-	private int version = 2; 		//2 bits
-	private int padding; 			//1 bit
-	private int itemCount;	 		//5 bits
-	private int packetType;			//8 bits
-	private long length;			//16 bits
+	protected boolean rawPktCurrent = false;
+	protected int version = 2; 		//2 bits
+	protected int padding; 			//1 bit
+	protected int itemCount;	 		//5 bits
+	protected int packetType;			//8 bits
+	protected int length;				//16 bits
 	
 	// Contains the actual data (eventually)
-	private byte[] rawPkt = null;
-	private byte[] information = null;
+	protected byte[] rawPkt = null;
 	
+	protected int getLength() {
+		return length;
+	}
 	
+	protected int parseHeaders() {
+		version = ((rawPkt[0] & 0xC0) >>> 6);
+		padding = ((rawPkt[0] & 0x20) >>> 5);
+		itemCount = (rawPkt[0] & 0x1F);
+		packetType = (int) rawPkt[1];
+		length = StaticProcs.combineBytes(rawPkt[2], rawPkt[3]);
+		
+		if(version == 2 && packetType < 205 && packetType > 199 && length < 65536) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
 	
 }
