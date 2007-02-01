af0c0489aa3914b172e52b3022b7dfd889b43ab3
arneke <arneke@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2007-02-01 07:57:11 +0000

    RTCP start

--- jlibrtp/CompRtcpPkt.java
+++ jlibrtp/CompRtcpPkt.java
@@ -1,9 +1,58 @@
 package jlibrtp;
 
-public class CompRtcpPkt {
-	private boolean rawPktCurrent = false;
-	private byte[] rawPkt = null;
-	private byte[][] rawRtcpPkts = null;
-	private RtcpPkt[] rtcpPkts;
+import java.util.*;
+import java.nio.ByteBuffer;
 
+public class CompRtcpPkt {
+	protected LinkedList<RtcpPkt> rtcpPkts = new LinkedList<RtcpPkt>();
+	protected CompRtcpPkt(RtcpPkt[] rtcpPackets ) {
+		
+		
+	}
+	
+	protected CompRtcpPkt(byte[] rawPkt) {
+		// Chop it up
+		int start = 0;
+		
+		while(start < (rawPkt.length-32)) {
+			int length = StaticProcs.combineBytes(rawPkt[start + 2], rawPkt[start + 3]);
+			byte[] tmpBuf = new byte[length];
+			int pktType = (int) rawPkt[start + 1];
+			System.arraycopy(rawPkt, start, tmpBuf, 0, length);
+			if(pktType == 200)
+				rtcpPkts.add(new RtcpPktSR(tmpBuf));
+			if(pktType == 201 )
+				rtcpPkts.add(new RtcpPktRR(tmpBuf));
+			if(pktType == 202)
+				rtcpPkts.add(new RtcpPktSDES(tmpBuf));
+			if(pktType == 203 )
+				rtcpPkts.add(new RtcpPktBYE(tmpBuf));
+			if(pktType == 204)
+				rtcpPkts.add(new RtcpPktAPP(tmpBuf));
+			start += length;
+		}
+	}
+	
+	protected byte[] encode() {
+		// We have to do it every time since we have no control over the packets.
+		ListIterator<RtcpPkt>  iter = rtcpPkts.listIterator();
+	
+		int rawPktSize = 0;
+		do {
+			RtcpPkt aPkt = iter.next();
+			rawPktSize += aPkt.getLength();
+		} while(iter.hasNext());
+		byte[] rawPkt = new byte[rawPktSize];
+		
+		int pos = 0;
+		iter = rtcpPkts.listIterator();
+		do {
+			RtcpPkt aPkt = iter.next();
+			System.arraycopy(aPkt.encode(), 0, rawPkt, pos, aPkt.getLength());
+			pos += aPkt.getLength();
+		} while(iter.hasNext());
+		
+		return rawPkt;
+	}
+	
 }
--- jlibrtp/RtcpPktRR.java
+++ jlibrtp/RtcpPktRR.java
@@ -1,5 +1,29 @@
 package jlibrtp;
 
 public class RtcpPktRR extends RtcpPkt {
-
+	
+	protected long reporterSsrc = -1; //32 bits
+	protected long reporteeSsrc = -1; //32 bits
+	protected int lossFraction = -1; //
+	protected int cumPacketsLost = -1;
+	protected long extHighSeqRecv = -1;
+	protected long interArvJitter = -1;
+	protected long timeStampLSR = -1;
+	protected long delaySR = -1;
+	
+	
+	protected RtcpPktRR(byte[] aRawPkt) {
+		rawPkt = aRawPkt;
+		
+		byte[] header = new byte[4];
+		System.arraycopy(aRawPkt, 0, header, 0, 4);
+		
+		if(super.parseHeaders() != 0 || packetType != 201) {
+			//Error...
+		}
+		
+		
+		
+		
+	}
 }
