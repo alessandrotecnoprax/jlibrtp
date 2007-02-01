d79b984db0b776755e33b35b0e32a3c0dd71267b
arneke <arneke@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2007-02-01 22:27:59 +0000

    tiny

--- jlibrtp/jlibrtp/RtcpPktRR.java
+++ jlibrtp/jlibrtp/RtcpPktRR.java
@@ -11,6 +11,11 @@ public class RtcpPktRR extends RtcpPkt {
 	protected long timeStampLSR = -1;
 	protected long delaySR = -1;
 	
+	protected RtcpPktRR(reporteeSsrc) {
+		// Fetch all the right stuff from the database
+		
+		
+	}
 	
 	protected RtcpPktRR(byte[] aRawPkt) {
 		rawPkt = aRawPkt;
@@ -21,9 +26,18 @@ public class RtcpPktRR extends RtcpPkt {
 		if(super.parseHeaders() != 0 || packetType != 201) {
 			//Error...
 		}
-		
-		
-		
+	
+		reporterSsrc = 		StaticProcs.combineBytes(aRawPkt[4],aRawPkt[5],aRawPkt[6],aRawPkt[7]);
+		reporteeSsrc = 		StaticProcs.combineBytes(aRawPkt[8],aRawPkt[9],aRawPkt[10],aRawPkt[11]);
+		lossFraction = 		(int) aRawPkt[12];
+		cumPacketsLost = 	(int) StaticProcs.combineBytes((byte) 0, aRawPkt[13],aRawPkt[14],aRawPkt[15]);
+		extHighSeqRecv = 	StaticProcs.combineBytes(aRawPkt[16],aRawPkt[17],aRawPkt[18],aRawPkt[19]);
+		interArvJitter = 	StaticProcs.combineBytes(aRawPkt[20],aRawPkt[21],aRawPkt[22],aRawPkt[23]);
+		timeStampLSR = 		StaticProcs.combineBytes(aRawPkt[24],aRawPkt[25],aRawPkt[26],aRawPkt[27]);
+		delaySR = 			StaticProcs.combineBytes(aRawPkt[28],aRawPkt[29],aRawPkt[30],aRawPkt[31]);
+	}
+	
+	protected byte[] encode() {
 		
 	}
 }
