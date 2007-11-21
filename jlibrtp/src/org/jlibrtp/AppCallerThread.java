382b42232640fccf57675874e0bff53e6217b51a
schnelle <schnelle@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2007-11-21 08:30:10 +0000

    Use of java.util.logging.

--- jlibrtp/src/org/jlibrtp/AppCallerThread.java
+++ jlibrtp/src/org/jlibrtp/AppCallerThread.java
@@ -19,6 +19,8 @@
 package org.jlibrtp;
 
 import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The purpose of this thread is to check whether there are packets ready from 
@@ -32,79 +34,83 @@ import java.util.Enumeration;
  * @author Arne Kepp
  */
 public class AppCallerThread extends Thread {
-	/**  The parent RTP Session */
-	RTPSession rtpSession;
-	/**  The applications interface, where the callback methods are called */
-	RTPAppIntf appl;
-	
-	/**
-	 * Instatiates the AppCallerThread
-	 * 
-	 * @param session the RTPSession with participants etc
-	 * @param rtpApp the interface to which data is given
-	 */
-	protected AppCallerThread(RTPSession session, RTPAppIntf rtpApp) {
-		rtpSession = session;
-		appl = rtpApp;
-		if(RTPSession.rtpDebugLevel > 1) {
-			System.out.println("<-> AppCallerThread created");
-		}  
-	}
-	
-	/**
-	 * The AppCallerThread will run in this loop until the RTPSession
-	 * is terminated.
-	 * 
-	 * Whenever an RTP packet is received it will loop over the
-	 * participants to check for packet buffers that have available
-	 * frame.
-	 */
-	public void run() {
-		if(RTPSession.rtpDebugLevel > 3) {
-			System.out.println("-> AppCallerThread.run()");
-		}
-		
-		while(rtpSession.endSession == false) {
-			
-			rtpSession.pktBufLock.lock();
-		    try {
-				if(RTPSession.rtpDebugLevel > 4) {
-					System.out.println("<-> AppCallerThread going to Sleep");
-				}
-				
-				try { rtpSession.pktBufDataReady.await(); } 
-					catch (Exception e) { System.out.println("AppCallerThread:" + e.getMessage());}
-					
-		    	// Next loop over all participants and check whether they have anything for us.
-				Enumeration<Participant> enu = rtpSession.partDb.getParticipants();
-				
-				while(enu.hasMoreElements()) {
-					Participant p = enu.nextElement(); 
-					
-					boolean done = false;
-					//System.out.println(p.ssrc + " " + !done +" " + p.rtpAddress 
-					//		+ " " + rtpSession.naiveReception + " " + p.pktBuffer);
-					//System.out.println("done: " + done + "  p.unexpected: " + p.unexpected);
-					while(!done && (!p.unexpected || rtpSession.naiveReception) 
-							&& p.pktBuffer != null && p.pktBuffer.length > 0) {
+    /** Logger instance. */
+    private static final Logger LOGGER =
+        Logger.getLogger(AppCallerThread.class.getName());
 
-						DataFrame aFrame = p.pktBuffer.popOldestFrame();
-						if(aFrame == null) {
-							done = true;
-						} else {
-							appl.receiveData(aFrame, p);
-						}
-					}
-				}
-		    
-		     } finally {
-		       rtpSession.pktBufLock.unlock();
-		     }
-			
-		}
-		if(RTPSession.rtpDebugLevel > 3) {
-			System.out.println("<- AppCallerThread.run() terminating");
-		}  
-	}
+    /**  The parent RTP Session */
+    RTPSession rtpSession;
+    /**  The applications interface, where the callback methods are called */
+    RTPAppIntf appl;
+
+    /**
+     * Instantiates the AppCallerThread
+     * 
+     * @param session the RTPSession with participants etc
+     * @param rtpApp the interface to which data is given
+     */
+    protected AppCallerThread(RTPSession session, RTPAppIntf rtpApp) {
+        rtpSession = session;
+        appl = rtpApp;
+        if(LOGGER.isLoggable(Level.FINEST)) {
+            LOGGER.finest("<-> AppCallerThread created");
+        }  
+    }
+
+    /**
+     * The AppCallerThread will run in this loop until the RTPSession
+     * is terminated.
+     * 
+     * Whenever an RTP packet is received it will loop over the
+     * participants to check for packet buffers that have available
+     * frame.
+     */
+    public void run() {
+        if(LOGGER.isLoggable(Level.FINER)) {
+            LOGGER.finer("-> AppCallerThread.run()");
+        }
+
+        while(rtpSession.endSession == false) {
+
+            rtpSession.pktBufLock.lock();
+            try {
+                if(LOGGER.isLoggable(Level.FINER)) {
+                    LOGGER.finer("<-> AppCallerThread going to Sleep");
+                }
+
+                try { rtpSession.pktBufDataReady.await(); } 
+                catch (Exception e) { System.out.println("AppCallerThread:" + e.getMessage());}
+
+                // Next loop over all participants and check whether they have anything for us.
+                Enumeration<Participant> enu = rtpSession.partDb.getParticipants();
+
+                while(enu.hasMoreElements()) {
+                    Participant p = enu.nextElement(); 
+
+                    boolean done = false;
+                    //System.out.println(p.ssrc + " " + !done +" " + p.rtpAddress 
+                    //		+ " " + rtpSession.naiveReception + " " + p.pktBuffer);
+                    //System.out.println("done: " + done + "  p.unexpected: " + p.unexpected);
+                    while(!done && (!p.unexpected || rtpSession.naiveReception) 
+                            && p.pktBuffer != null && p.pktBuffer.length > 0) {
+
+                        DataFrame aFrame = p.pktBuffer.popOldestFrame();
+                        if(aFrame == null) {
+                            done = true;
+                        } else {
+                            appl.receiveData(aFrame, p);
+                        }
+                    }
+                }
+
+            } finally {
+                rtpSession.pktBufLock.unlock();
+            }
+
+        }
+        if(LOGGER.isLoggable(Level.FINER)) {
+            LOGGER.finer("<- AppCallerThread.run() terminating");
+        }  
+    }
 
 }
