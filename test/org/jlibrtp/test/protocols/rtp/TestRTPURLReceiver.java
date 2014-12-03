a202d18833e3d202f19320b3ad8e77ef1f257520
schnelle <schnelle@aa9ace9b-8e23-0410-a8c2-d117cb369214>
2014-12-03 13:19:45 +0000

    Updated license info to meet the points named in https://bugzilla.redhat.com/show_bug.cgi?id=1167076

--- test/org/jlibrtp/test/protocols/rtp/TestRTPURLReceiver.java
+++ test/org/jlibrtp/test/protocols/rtp/TestRTPURLReceiver.java
@@ -1,20 +1,38 @@
+/**
+ * Java RTP Library (jlibrtp)
+ * Copyright (C) 2006 Arne Kepp
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.jlibrtp.test.protocols.rtp;
 
-import java.net.URL;
-import java.io.InputStream;
-import java.net.URLConnection;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.logging.Logger;
-import java.util.logging.Level;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import javax.sound.sampled.SourceDataLine;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine.Info;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.Line;
-import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.SourceDataLine;
 
 /**
  * <p>Title: jlibrtp</p>
